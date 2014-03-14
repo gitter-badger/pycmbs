1f037888c739a030174d654b19906fd5c83944b7
Mikhail Itkin <itkin@driftnoise.com>
2014-03-14 13:49:29 +0100

    [bench/config2] added json dict parsing
    Skip yaml, as json is a yaml subset
    and json is already required anyway
    keeping the list of dependencies the same

--- pycmbs/benchmarking/config2.py
+++ pycmbs/benchmarking/config2.py
@@ -5,6 +5,39 @@ For COPYING and LICENSE details, please refer to the file
 COPYRIGHT.md
 """
 
+import json
+
+class ConfigFile(object):
+    """
+    """
+    pass
 
 def load_config(filename):
+    """
+    Load benchmarking configuration from file
+    """
+    pass
+
+def parse_config(config_string, fmt='json'):
+    """
+    Parse config from string
+    Allow choice between config storage backends
+    """
+    if fmt is 'json':
+        config_contents = parse_json_config(config_string)
+    else:
+        raise ValueError("config format not recognized")
+    return config_contents
+
+def parse_json_config(config_string):
+    """
+    Parse JSON file
+    """
+    config_contents = json.loads(config_string)
+    return config_contents
+    
+def save_config(filename):
+    """
+    Save benchmarking configuration to file
+    """
     pass
