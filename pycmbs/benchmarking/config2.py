667f0dc0b9caf7d62ab8f710382a3a89d9640cb6
Mikhail Itkin <itkin@driftnoise.com>
2014-03-14 14:16:54 +0100

    [config2] moved yaml and json imports to methods

--- pycmbs/benchmarking/config2.py
+++ pycmbs/benchmarking/config2.py
@@ -4,9 +4,6 @@ This file is part of pyCMBS. (c) 2012-2014
 For COPYING and LICENSE details, please refer to the file
 COPYRIGHT.md
 """
-
-import json
-
 class ConfigFile(object):
     """
     """
@@ -25,6 +22,8 @@ def parse_config(config_string, fmt='json'):
     """
     if fmt is 'json':
         config_contents = parse_json_config(config_string)
+    elif fmt is 'yaml':
+        config_contents = parse_yaml_config(config_string)
     else:
         raise ValueError("config format not recognized")
     return config_contents
@@ -33,9 +32,18 @@ def parse_json_config(config_string):
     """
     Parse JSON file
     """
+    import json
     config_contents = json.loads(config_string)
     return config_contents
     
+def parse_yaml_config(config_string):
+    """
+    Parse YAML string
+    """
+    import yaml
+    config_contents = yaml.load(config_string)
+    return config_contents
+
 def save_config(filename):
     """
     Save benchmarking configuration to file
